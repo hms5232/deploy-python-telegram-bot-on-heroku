a361bd2e9eb0c29e44658940c7cc2ffbc26e18a7
hms5232 <hms5232@yahoo.com.tw>
2020-05-20 22:32:11 +0800

    在準備部署 Heroku 之前
    也就是在你開發完成機器人的狀態

---
+++ bot.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+"""
+Author：hms5232
+Repo：https://github.com/hms5232/deploy-python-telegram-bot-on-heroku
+"""
+
+
+from configparser import ConfigParser
+
+from telegram.ext import Updater, CommandHandler
+
+
+# 設定一些個人的環境變數
+env = ConfigParser()
+env.read('config.ini')
+# If you don't want use config.ini, please edit following variables for your environment.
+TOKEN = env['bot']['TOKEN']
+
+
+updater = Updater(token=TOKEN)  # 呼叫 bot 用
+
+
+def welcome(bot, update):
+	""" Show user welcome message. """
+	
+	chat_id = update.message.from_user.id
+	
+	about_bot = ''
+	about_bot = about_bot + 'Hello! 感謝您的使用\n'
+	about_bot = about_bot + '本機器人由 [hms5232](https://medium.com/@hms5232) 開發\n'
+	about_bot = about_bot + '用於將機器人部署在 Heroku 上的教學文的範例\n'
+	about_bot = about_bot + '可於 [Github](https://github.com/hms5232) 上找到我\n'
+	about_bot = about_bot + '文章原文可在[repo簡介](https://github.com/hms5232/deploy-python-telegram-bot-on-heroku)上找到\n'
+	
+	bot.send_message(chat_id, about_bot, parse_mode='Markdown')
+
+
+def hello(bot, update):
+	""" Hello World! """
+	
+	update.message.reply_text('Hello world!')
+
+
+updater.dispatcher.add_handler(CommandHandler('start', welcome))  # 歡迎訊息
+updater.dispatcher.add_handler(CommandHandler('hi', hello))  # Hello World!
+
+
+# 執行機器人必須要的，讓機器人運作聽命
+updater.start_polling()
+updater.idle()
